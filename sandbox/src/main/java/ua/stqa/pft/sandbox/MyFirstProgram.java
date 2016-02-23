ff8c76228f3cc98b9af6e738ee03b9bb0ff96492
Taras M <training096@mail.ru>
2016-02-23 18:47:13 +0200

    Второй способ решения задания № 2 (вызов метода с помощью объекта)

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,22 +1,15 @@
 package ua.stqa.pft.sandbox;
 
-public class MyFirstProgram{
-	
-	public static void main(String[] args){
+public class MyFirstProgram {
 
+  public static void main(String[] args) {
 
 
     Point point1 = new Point(3, 4);
     Point point2 = new Point(7, 8);
 
-    System.out.println("Расстояние между точками равно " + distance(point1, point2));
+    System.out.println("Расстояние между точками равно " + point1.distance(point1, point2));
 
   }
 
-  public static double distance(Point p1, Point p2){
-
-    return  Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-
-  }
-
-}                         
+}
