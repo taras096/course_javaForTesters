4ab4534e7dca36d31b071cd809a113abe0fc594c
Taras M <training096@mail.ru>
2016-02-23 18:41:56 +0200

    Первый способ решения задания №2 (вызов статического метода)

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,20 @@ package ua.stqa.pft.sandbox;
 public class MyFirstProgram{
 	
 	public static void main(String[] args){
-		hello("world");
-    hello("user");
-    hello("Taras");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point point1 = new Point(3, 4);
+    Point point2 = new Point(7, 8);
+
+    System.out.println("Расстояние между точками равно " + distance(point1, point2));
+
   }
 
-  public static void hello(String somebody){
-    System.out.println("Hello, " + somebody + "!");
+  public static double distance(Point p1, Point p2){
+
+    return  Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+
   }
 
 }                         
---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
@@ -0,0 +1,20 @@
+package ua.stqa.pft.sandbox;
+
+/**
+ * Created by mareev on 23.02.2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+
+
+
+}
+
