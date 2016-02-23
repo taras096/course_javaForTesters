6d91570ce6bf730440bd3e861aeb77c54ab1f622
Taras M <training096@mail.ru>
2016-02-23 13:09:23 +0200

    добавились классы Rectangle и Square (все изменения после 15 урока)

---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,19 @@
+package ua.stqa.pft.sandbox;
+
+/**
+ * Created by mareev on 23.02.2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area (){
+    return this.a * this.b;
+  }
+}
---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ua.stqa.pft.sandbox;
+
+/**
+ * Created by mareev on 23.02.2016.
+ */
+public class Square {
+
+  public double l;
+  
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area (){
+    return this.l * this.l;
+  }
+
+}
