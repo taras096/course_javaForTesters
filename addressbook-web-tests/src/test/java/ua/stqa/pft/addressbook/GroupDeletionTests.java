24642ffc8833e14ecaf242422913a0da1303cbb8
Taras M <training096@mail.ru>
2016-03-10 16:05:49 +0200

    Урок 2. Видео 8
    1. Создал GroupDeletionTests класс для удаления группы
    2. Создал TestBase класс, куда вынес все вспомогательные методы

---
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ua.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase{
+
+    
+    @Test
+    public void testGroupDeletion() {
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
